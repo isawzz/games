05af5fe5738b3b96481987c2e7d0beb66946c919
isawzz <iwgajam@gmail.com>
2020-09-02 05:37:33 -0700

    noch1

--- CODE/globalConfig.js
+++ CODE/globalConfig.js
@@ -1,2 +1,2 @@
 const TIMIT_SHOW = false; // true | false
-const USE_LOCAL_STORAGE = false; // true | false //localStorage is cleared when false!!!!!
+const USE_LOCAL_STORAGE = true; // true | false //localStorage is cleared when false!!!!!
