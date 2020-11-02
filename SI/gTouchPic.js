b61b668f1ae38893e3cb7eeb81a4cf528676844b
isawzz <iwgajam@gmail.com>
2020-11-02 11:12:24 -0800

    das

--- SI/gTouchPic.js
+++ SI/gTouchPic.js
@@ -1,10 +1,20 @@
-function initTP(){}
+function initTP(){
+	NumPics = 2+level;
+	keySet = getKeySet(WORD_GROUPS[iGROUP], currentLanguage, MAX_WORD_LENGTH[level]);
+
+}
 function roundTP(){}
 function promptTP(){
-	console.log('should prompt TouchPic');
+	showPictures(true,() => mBy(defaultFocusElement).focus());
+	setGoal(true);
 
-	NumPics = 2 + level;
+	showInstruction(bestWord, currentLanguage == 'E' ? 'type' : "schreib'", dTitle);
 
+	mLinebreak(dTable);
+	inputBox = addNthInputElement(dTable,trialNumber);
+	defaultFocusElement = inputBox.id;
+
+	return 10;
 }
 function activateTP(){
 	console.log('should activate TouchPic UI')
