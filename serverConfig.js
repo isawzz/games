e1cd025b152c694924ccff3e61fe49fc2e293e3c
isawzz <iwgajam@gmail.com>
2020-12-02 15:47:03 -0800

    das

--- serverConfig.js
+++ serverConfig.js
@@ -2,6 +2,6 @@ const OFFLINE = true;
 // const SERVERURL =  OFFLINE ? 'http://localhost:3000/users/' : 'https://speech-games.herokuapp.com/users/';
 const SERVERURL =  OFFLINE ? 'http://localhost:3000/app/' : 'https://speech-games.herokuapp.com/app/';
 const BROADCAST_SETTINGS = false; // ACHTUNG!!!!!!!!!!!!! true;
-const EXPERIMENTAL = true;
+const EXPERIMENTAL = false;
 const DEV_MODE = false; //OFFLINE is true auf ASUS ROG soll aber false sein auf github!
 const IS_TESTING = false;
