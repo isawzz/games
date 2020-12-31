e77a3664f5eb796361f50f53d55742cf0d7e84e5
isawzz <iwgajam@gmail.com>
2020-12-31 10:40:26 -0800

    das

--- TEST/t94/start.js
+++ TEST/t94/start.js
@@ -2,15 +2,57 @@ window.onload = start;
 
 function start() {
 	console.log('start 94');
-	
+
+	test02();
+
+
 	//test01();
 }
-function test01(){
+var DragSource = null; var DragElem = null;
+function onMovingCloneAround(ev) {
+
+}
+function onMouseDownOnLetter(ev) {
+	ev.preventDefault();
+	let id = evToClosestId(ev);
+	console.log('mouse down on', id);
+	let d = mBy(id);
+	DragSource = d;
+
+	//d wird gecloned
+	var clone = DragElem = DragSource.cloneNode(true);
+
+	//clone muss an body attached werden
+	mAppend(document.body, clone);
+	mClass(clone, 'letter')
+
+	//der clone muss class 'dragelem' sein
+	mClass(clone, 'dragelem');
+
+	//der clone wird richtig plaziert
+	mStyleX(clone, { left: ev.clientX - ev.offsetX, top: ev.clientY - ev.offsetY });
+
+	// von jetzt an un solange DragElem != null ist muss der clone sich mit der maus mitbewegen
+	document.body.onmousemove = onMovingCloneAround;
+
+	//der clone bekommt ein 
+}
+function test02() {
+
+	let letters = Array.from(mBy('dLetters').children);
+	console.log('letters', letters);
+	for (const d of letters) {
+		d.onmousedown = onMouseDownOnLetter;
+	}
+
+
+}
+function test01() {
 	let source = mBy('d1');
-	mStyleX(source,{bg:'pink',cursor:'pointer',display:'inline'})
+	mStyleX(source, { bg: 'pink', cursor: 'pointer', display: 'inline' })
 	source.onmousedown = onDragElementMouseDown;
 }
-function test00(){
+function test00() {
 	let source = mBy('d1');
 	var clone = DClone = source.cloneNode(true);
 	// clone.classList.add("dragging");
