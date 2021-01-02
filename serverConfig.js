29259501c9d2177eb106db430ede00ea2e279fee
isawzz <iwgajam@gmail.com>
2021-01-02 05:58:42 -0800

    das

--- serverConfig.js
+++ serverConfig.js
@@ -1,4 +1,4 @@
-const OFFLINE = true;
+const OFFLINE = false;
 DEFAULTUSERNAME = 'nil';
 // const SERVERURL =  OFFLINE ? 'http://localhost:3000/users/' : 'https://speech-games.herokuapp.com/users/';
 const SERVERURL =  OFFLINE ? 'http://localhost:3000/app/' : 'https://speech-games.herokuapp.com/app/';
