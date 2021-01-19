0db757082a8a467ab274c7687db9905e3eaf701d
isawzz <iwgajam@gmail.com>
2021-01-19 07:03:26 -0800

    ref

--- serverConfig.js
+++ serverConfig.js
@@ -1,10 +1,10 @@
-const OFFLINE = true;
+const OFFLINE = false;
 const SERVERURL = OFFLINE ? 'http://localhost:3000/app/' : 'https://speech-games.herokuapp.com/app/';
 
-const IS_TESTING = false; // *** only set this one! ***
+const IS_TESTING = true; // *** only set this one! ***
 
 DEFAULTUSERNAME = 'nil';// IS_TESTING ? 'nil' : 'gul';
-const BROADCAST_SETTINGS = false; //IS_TESTING; // ACHTUNG!!!!!!!!!!!!! true;
+const BROADCAST_SETTINGS = true; //IS_TESTING; // ACHTUNG!!!!!!!!!!!!! true;
 var USE_LOCAL_STORAGE = !BROADCAST_SETTINGS; // true | false //localStorage is cleared when false!!!!!
 const CLEAR_LOCAL_STORAGE = BROADCAST_SETTINGS;
 var PROD_START = !IS_TESTING;
