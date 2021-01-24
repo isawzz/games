223088134c7b3e81cf29a3f2c6d2a2d36d7b73fc
isawzz <iwgajam@gmail.com>
2021-01-24 08:50:52 -0800

    das

--- DIE/controller0.js
+++ DIE/controller0.js
@@ -1,60 +1,63 @@
 var Items = [];
 var Zones = {};
 
-function clearZones(){
-	for(const k in Zones){
+//#region zones
+function clearZones() {
+	for (const k in Zones) {
 		clearElement(mBy(Zones[k].idData));
 	}
 }
+function createPlayerZone(pl, namePos = 'top') {
+	let z = { id: pl.id };
+	let d = z.div = mDiv(dTable, { align: 'center' });//,{display:'flex',bg:pl.color});
+	d.id = 'zone_' + pl.id;
+	if (namePos == 'top') {
+		z.dName = mText(pl.id, d, { display: 'inline-block', maleft: -10 });//,bg:'red'});
+	}
+	z.dData = mDiv(d, { margin: 0 });//,bg:'green'});
+	if (namePos == 'bottom') {
+		z.dName = mText(pl.id, d, { display: 'inline-block', maleft: -10 });//,bg:'red'});
+	}
+	z.dData.id = z.idData = 'data_' + pl.id;
+	return z;
+}
+function takeYourSeats() {
+	console.log('players taking seat', T.numPlayers)
+	Zones = {};
+	if (T.numPlayers == 2) {
+
+		Zones[T.players[0].id] = createPlayerZone(T.players[0]);
+		mLinebreak(dTable);
+		let z = Zones.table = { id: 'table', div: mDiv(dTable, { hmin: 120 }) };
+		z.div.id = z.idData = 'data_' + table;
+		mLinebreak(dTable);
+		Zones[T.players[1].id] = createPlayerZone(T.players[1], 'bottom');
+	}
+	console.log('Zones', Zones)
+}
+//#endregion
+
+//#region ablauf
 function startGame() {
 	if (nundef(T.running)) setup();
 	if (Settings.perspective != 'me') takeYourSeats();
 	startTurn();
 }
-function createPlayerZone(pl,namePos='top'){
-	let z={id:pl.id};
-	let d=z.div=mDiv(dTable,{align:'center'});//,{display:'flex',bg:pl.color});
-	d.id='zone_'+pl.id;
-	if (namePos == 'top'){
-		z.dName = mText(pl.id,d,{display:'inline-block',maleft:-10});//,bg:'red'});
-	}
-	z.dData = mDiv(d,{margin:0});//,bg:'green'});
-	if (namePos == 'bottom'){
-		z.dName = mText(pl.id,d,{display:'inline-block',maleft:-10});//,bg:'red'});
-	}
-	z.dData.id=z.idData='data_'+pl.id;
-	return z;
-}
-function takeYourSeats(){
-	console.log('players taking seat',T.numPlayers)
-	Zones = {};
-	if (T.numPlayers == 2){
-
-		Zones[T.players[0].id]=createPlayerZone(T.players[0]);
-		mLinebreak(dTable);
-		let z=Zones.table={id:'table',div:mDiv(dTable,{hmin:120})};
-		z.div.id=z.idData='data_'+table;
-		mLinebreak(dTable);
-		Zones[T.players[1].id]=createPlayerZone(T.players[1],'bottom');
-	}
-	console.log('Zones',Zones)
-}
 function startTurn() {
 	let me = getTurnPlayer();
 	changeUserTo(me.id)
 	showHands();
-
 	present();
 	optionsFor(me);
 }
-function present(){
+function present() {
 	if (Settings.perspective == 'me') presentFor(me);
 	else presentAll();
 }
-function presentAll(){
+function presentAll() {
 	clearZones();
-	console.log('Zones',Zones)
-	for(const pl of T.players){
+	console.log('Zones', Zones)
+	for (const pl of T.players) {
 		let zone = Zones[pl.id];
 		pl.hand.showDeck(zone.dData, 'right', 0, false);
 	}
@@ -86,11 +89,27 @@ function presentFor(me) {
 	// activateUi();
 
 }
-function topCardShouldGoTo(deck0, deck1, faceUp) {
-	let c = Deck.transferTopFromTo(deck0,deck1)
-	if (faceUp == true) Card52.turnFaceUp(c);
-	mRemoveStyle(c.div, ['cursor', 'position']);
+function optionsFor(me) {
+	activateOn(me.hand.topCard(), 'onclick', () => topCardShouldGoTo(me.hand, T.trick, true));
 }
+function evaluate() {
+	T.index = (T.index + 1) % T.players.length;
+	//once each player had a turn, turn outcome, start new round!
+	if (T.index) startTurn(); else endTurn();
+}
+function endTurn(){
+	//look which card is higher in rank
+	let res = indexOfMax(T.trick,);
+	for(let i=0;i<T.trick.length;i++){
+		
+	}
+	//check which player's card it was
+	//add both cards to this player's hand
+	//or start a war
+}
+//#endregion
+
+//#region helpers
 function activateOn(item, event, handler) {
 	console.log(item)
 	let d = item.div;
@@ -98,41 +117,42 @@ function activateOn(item, event, handler) {
 	d[event] = ev => { handler(ev); evaluate() };
 	item.isActive = true;
 }
-function optionsFor(me) {
-	activateOn(me.hand.topCard(), 'onclick', () => topCardShouldGoTo(me.hand, T.trick, true));
-}
-
-function evaluate() {
-	T.index = (T.index + 1) % T.players.length;
-	startTurn();
-}
-
-
 function addItem(owner, key, val) {
 	let o = owner[key] = val;
 	Items.push(o);
-	console.log('Items',Items)
-}
-function showHands(){
-	for (const pl of T.players) {
-		console.log('hand',pl.id,pl.hand.map(x=>x.key))
-	}
-	console.log('trick',T.trick.map(x=>x.key))
-}
-function setup() {
-	let deck = new Deck();
-	deck.initTest(6);
-	let n=deck.count()/2;
-	T.numPlayers = T.players.length;
-	for (const pl of T.players) {
-		pl.hand = deck.deal(n);
-		pl.warZone = [];		//each player's warzone is empty
-	}
-	T.trick = new Deck();T.trick.initEmpty(); //in the middle of the table there is an empty trick
-	shuffle(T.players); //player order is random
-	T.index = 0;
-	T.running = true;
+	console.log('Items', Items)
 }
 function getTurnPlayer() {
 	return T.players[T.index];
 }
+function showHands() {
+	for (const pl of T.players) {
+		console.log('hand', pl.id, pl.hand.map(x => x.key))
+	}
+	console.log('trick', T.trick.map(x => x.key))
+}
+function setup() {
+	let deck = new Deck();
+	deck.initTest(6);
+	let n = deck.count() / 2;
+	T.numPlayers = T.players.length;
+	for (const pl of T.players) {
+		addItem(pl,'hand',deck.deal(n));
+		//pl.hand = deck.deal(n);
+		addItem(pl,'warZone',[]);
+
+		//pl.warZone = [];		//each player's warzone is empty
+	}
+	addItem(T,'trick',new Deck());
+	//T.trick = new Deck(); 
+	T.trick.initEmpty(); //in the middle of the table there is an empty trick
+	shuffle(T.players); //player order is random
+	T.index = 0;
+	T.running = true;
+}
+function topCardShouldGoTo(deck0, deck1, faceUp) {
+	let c = Deck.transferTopFromTo(deck0, deck1)
+	if (faceUp == true) Card52.turnFaceUp(c);
+	mRemoveStyle(c.div, ['cursor', 'position']);
+}
+//#endregion
